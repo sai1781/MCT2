e54c69832d69f979e28f62bee6e883085e23e013
sai1781 <96867450+sai1781@users.noreply.github.com>
2022-11-21 19:57:19 +0530

    Update TextContainer.js

--- src/Components/TextContainer.js
+++ src/Components/TextContainer.js
@@ -16,7 +16,7 @@ const TextContainer = _ => {
 
 
     return (
-        <div className="textContainer" style={{ color: value2, fontSize: `${value3}px`, textAlign: value4 }} >
+         <div className="textContainer" style={{ color: value2, fontSize: `${value3}px`, fontFamily:`${value4}` }} >
             <div className="text" >
                 <p >It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)</p>
                 <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)</p>
